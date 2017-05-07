d70cb56332dc0a7d20716aabf5c8721e01aabb30
Ninad <khyati.ninad@gmail.com>
2017-05-08 01:04:49 +0530

    CSS Fix

--- visualizations/KhyatiNinad/booklet/1.0.0/booklet/js/booklet.js
+++ visualizations/KhyatiNinad/booklet/1.0.0/booklet/js/booklet.js
@@ -1,4 +1,50 @@
+    var module = {
+        ratio: 1.6,
+        init: function (id) {
+            var me = this;
 
+            // if older browser then don't run javascript
+            if (document.addEventListener) {
+                this.el = document.getElementById(id);
+                this.resize();
+
+                // on window resize, update the plugin size
+                window.addEventListener('resize', function (e) {
+                    var size = me.resize();
+                    $(me.el).turn('size', size.width, size.height);
+                });
+            }
+        },
+        resize: function () {
+            // reset the width and height to the css defaults
+            this.el.style.width = '';
+            this.el.style.height = '';
+
+            var width = this.el.clientWidth,
+                height = Math.round(width / this.ratio),
+                padded = Math.round(document.body.clientHeight * 0.9);
+
+            // if the height is too big for the window, constrain it
+            if (height > padded) {
+                height = padded;
+                width = Math.round(height * this.ratio);
+            }
+
+            // set the width and height matching the aspect ratio
+            this.el.style.width = width + 'px';
+            this.el.style.height = height + 'px';
+
+            return {
+                width: width,
+                height: height
+            };
+        },
+        plugins: function () {
+            // run the plugin
+            // hide the body overflow
+            document.body.className = 'hide-overflow';
+        }
+    };
 function updateDepth(book, newPage) {
 
 	var page = book.turn('page'),
@@ -535,6 +581,8 @@ function loadApp() {
 
     $('#canvas').css({ visibility: '' });
     Handlers.ready();
+    module.init('book');
+
 //    setTimeout(setImageViewPointHeight, 500);
 
 }
@@ -683,3 +731,5 @@ function resize() {
 
     $('.sj-book').turn('size', size.width, size.height);
 }
+
+
